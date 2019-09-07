8e9a9633646cdda19237a49b518bcb244341ef86
bruno Ezemba <brunoaugustine2@gmail.com>
2019-09-07 12:59:37 +0100

    updated the js file

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ const screen = document.querySelector('.screen')
 const equalBtn = document.querySelector('.btn-equal')
 const clearBtn = document.querySelector('.btn-clear')
 
-
-
 for(let i = 0; i < btns.length; i++){
-	btns[i].addEventListener('click', function(){
-let number = btns[i].getAttribute('data-num');
-		screen.value += number;
+	btns[i].addEventListener('click', () => {
+	let number = btns[i].getAttribute('data-num');
+	screen.value += number;
 })
 }
-equalBtn.addEventListener('click', function(){
+equalBtn.addEventListener('click', () => {
 	if(screen.value === ''){
 		alert('input is empty')
 	}else{
@@ -20,6 +18,6 @@ equalBtn.addEventListener('click', function(){
 		};
 });
 
-clearBtn.addEventListener('click', function(){
+clearBtn.addEventListener('click', () => {
 	screen.value = '';
 });
