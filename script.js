4a17f171f648bc391c552547edec680f05c7307b
bruno Ezemba <brunoaugustine2@gmail.com>
2019-09-07 13:09:50 +0100

    updated the js file

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ const screen = document.querySelector('.screen')
 const equalBtn = document.querySelector('.btn-equal')
 const clearBtn = document.querySelector('.btn-clear')
 
-/*for(let i = 0; i < btns.length; i++){
-	btns[i].addEventListener('click', () => {
-	let number = btns[i].getAttribute('data-num');
-	screen.value += number;
-})
-}*/
 btns.forEach(btn => {
 	btn.addEventListener('click', () => {
 		let number = btn.getAttribute('data-num')
